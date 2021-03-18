dc83b0d8eabae96723ffd03e44d366f7198a9778
pixelboogie <41452340+pixelboogie@users.noreply.github.com>
2021-03-18 09:55:25 -0700

    Add files via upload

---
+++ Delegates.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delegates : MonoBehaviour
+{
+
+    // accessor, keyword delegate, define return type, name signature, parameters
+    public delegate void TestDelegate();
+
+    private TestDelegate testDelegateFunction;
+
+    void Start()
+    {
+        
+        testDelegateFunction = MyTestDelegateFunction;
+
+        testDelegateFunction += MyTestDelegateFunction2;
+
+        testDelegateFunction();
+
+        testDelegateFunction -= MyTestDelegateFunction2;
+
+        testDelegateFunction();
+
+    }
+
+    private void MyTestDelegateFunction()
+    {
+        Debug.Log("Executing - MyTestDelegateFunction");
+    }
+
+    private void MyTestDelegateFunction2()
+    {
+        Debug.Log("Executing - MyTestDelegateFunction2");
+    }
+
+}
